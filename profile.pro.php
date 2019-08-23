7ba900c1207890d33e44990a00d5e52a7eaf6d4c
Proxy <atcav1@outlook.com>
2019-08-23 10:05:59 +1000

    Profile class adding and removing in progress

--- profile.pro.php
+++ profile.pro.php
@@ -6,7 +6,19 @@ if (!empty($_SESSION['user'] && $_SESSION['start'] && $_SESSION['cdid'] && $_SES
   require_once("./mydb/databaseManager/DBEnter.db.php");
   ?>
  <script>
+   $(document).ready(function() {
+     console.log( "ready!" );
+     $("#overview").load("./mydb/profile/Overview.pro.php");
 
+     $("#btnOverview").click(function () {
+       $("#overview").removeClass("d-none");
+       $("#accountSettings").addClass("d-none");
+       $("#savedInventory").addClass("d-none");
+       $("#help").addClass("d-none");
+     });
+
+
+   });
  </script>
  <main role="main" class="container" style="padding-top: 83px;">
    <div class="row">
@@ -14,22 +26,33 @@ if (!empty($_SESSION['user'] && $_SESSION['start'] && $_SESSION['cdid'] && $_SES
      <section class="col-3 border">
        <div>
          <!-- Users IMG -->
-         <img src="img/profileIMG/11e79c5bd7cf103d.png" class="w-75 d-block mx-auto rounded">
+         <img src="img/profileIMG/11e79c5bd7cf103d.png" class="w-75 d-block mx-auto rounded" alt="Profile Image">
          <h5 class="text-center">Username</h5>
        </div>
        <div class="w-75 d-block mx-auto">
          <ul class="list-group">
-           <li><a href="#" class="list-group-item btn btn-outline-primary">Overview</a></li>
-           <li><a href="#" class="list-group-item btn btn-outline-dark">Account Settings</a></li>
-           <li><a href="#"class="list-group-item btn btn-outline-dark">Saved Inventory</a></li>
-           <li><a href="#" class="list-group-item btn btn-outline-info">Help</a></li>
+           <li><a id="btnOverview" class="list-group-item btn btn-outline-primary">Overview</a></li>
+           <li><a id="btnAccountSettings" class="list-group-item btn btn-outline-dark">Account Settings</a></li>
+           <li><a id="btnSavedInventory" class="list-group-item btn btn-outline-dark">Saved Inventory</a></li>
+           <li><a id="btnHelp" class="list-group-item btn btn-outline-info">Help</a></li>
          </ul>
        </div>
      </section>
      <!-- right side of page -->
      <section class="col-9 border">
        <div>
-
+         <div id="overview" class="">
+           <!-- overview section -->
+         </div>
+         <div id="accountSettings" class="d-none">
+           <!-- Account Settings Section -->
+         </div>
+         <div id="savedInventory" class="d-none">
+          <!-- Saved Inventory Section -->
+         </div>
+         <div id="help" class="d-none">
+           <!-- help section -->
+         </div>
        </div>
      </section>
    </div>
