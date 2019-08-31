79b8c7c178295306da6cdbb46b61c8b8ce6f1aef
Proxy Fox <atcav1@outlook.com>
2019-08-31 22:54:29 +1000

    Form for users to add to the data review thing

--- gameContent.php
+++ gameContent.php
@@ -15,7 +15,18 @@ $resultCarouselImg = DB::query("
     AND IMG REGEXP 'IMG'
 ");
 
+$resultGameQuery = DB::queryFirstRow("
+  SELECT *
+  FROM gameinventory 
+  WHERE GIID = '".$_GET['GIID']."'
+");
 
+$resultUserReview = DB::query("
+  SELECT usergamecomment.date, usergamecomment.comment, clientprofile.displayName, clientprofile.UIMG
+  FROM usergamecomment RIGHT JOIN clientprofile
+    ON usergamecomment.CPID = clientprofile.CPID
+  ORDER BY RAND() LIMIT 6
+");
 
 ?>
 
@@ -50,6 +61,7 @@ $resultCarouselImg = DB::query("
       $("#delivery").toggleClass("d-none");//toggle class on click
     });
 
+    // Overview Review and specification swapper
     $("#btnOverview").click(function () {
       $("#overview").removeClass('d-none');
       $("#reviews").addClass('d-none');
@@ -67,12 +79,55 @@ $resultCarouselImg = DB::query("
     });
 
 
+    $("#btnUserComment").click(function () {
+      $('#spinner0').addClass('spinner-border spinner-border-sm');
+      $.post("./mydb/profile/content/gameContent.worker.php", {
+        comment:     $("#userComment").val(),
+        GIID:        $("#formGIID").val(),
+      },
+      function (data, status) {
+        $('#spinner0').removeClass('spinner-border spinner-border-sm');
+        $("#userComment").val("");
+        console.log(data, status);
+        if (data === "fail20") {
+          $("#generalMessage").text("No New Values");
+        } else if (data === "success2") {
+          $("#generalMessage").text("Data Updated");
+        } else {
+          $("#generalMessage").text("Server Error");
+        }
+      })
+    });
+
   });
 </script>
 
 <main role="main" class="container" style="padding-top: 90px; margin-bottom: 50px;">
-  <h1 itemprop="name">Game Name</h1>
-  <a class="btn btn-link" style="color: gray;" href="#"><small itemprop="brand">Item location</small></a>
+  <h1 itemprop="name"><?php echo $resultGameQuery['productName'] ?></h1>
+  <a
+      class="btn btn-link"
+      style="color: gray;"
+      href="<?php
+        // make sure the user goes to the correct location of the device
+        // 1 = PS4, 2 = xBox, 3 = Nintendo, 4 = Personal Computer
+        // and anything bigger means it supports multiple devices
+        if ($resultGameQuery['platForm'] == 1){
+          echo "PS4.php";
+        } elseif ($resultGameQuery['platForm'] == 2) {
+          echo "xbox.php";
+        }
+        elseif ($resultGameQuery['platForm'] == 3) {
+          echo "nintendo.php";
+        }
+        elseif ($resultGameQuery['platForm'] == 4) {
+          echo "PC.php";
+        } else {
+          echo "index.php";
+        }
+      ?>"
+  >
+    <small itemprop="brand">Item location</small>
+  </a>
 
   <section class="row">
     <!-- left side Image selector -->
@@ -92,6 +147,7 @@ $resultCarouselImg = DB::query("
             FROM gameinventoryimg 
             WHERE GIID = '".$_GET['GIID']."'
               AND IMG REGEXP 'IMG'
+            ORDER BY RAND()
           ");
 
           //assigning arrays
@@ -167,7 +223,7 @@ $resultCarouselImg = DB::query("
     <!-- right side Product saving and small information -->
     <div class="col-md-4 container">
       <!-- Price of the product -->
-      <h2 itemprop="price">$500</h2>
+      <h2 itemprop="price">$<?php echo $resultGameQuery['price'] ?></h2>
       <hr>
       <div class="row">
         <div class="col-10">
@@ -211,116 +267,154 @@ $resultCarouselImg = DB::query("
         <a class="navbar-brand" id="btnOverview">Overview</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>
+        </button><!-- Navbar toggle end -->
 
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
               <a class="nav-link" id="btnReviews">Reviews</a>
-            </li>
+            </li><!-- nav item end -->
             <li class="nav-item">
               <a class="nav-link" id="btnSpecification">Specifications</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+            </li> <!-- nav item end -->
+          </ul><!-- navbar nav end -->
+        </div><!-- collapse end -->
+      </nav><!-- navbar end -->
 
       <!-- Overview section -->
       <div id="overview" class="">
         <h3>Short Description</h3>
-        <p>Far Cry 5 is a massive gameplay arena that's filled with something new around every bend. The enemy AI behavior is more realistic, and the exploration is endless. Even when you feel like taking a break from the campaign, you can take in some leisurely fishing before diving back into your quest. It's your mission.</p>
+        <p><?php echo $resultGameQuery['briefDescription']; ?></p>
         <br>
         <h3>Long Description</h3>
-        <p>Far Cry 5 is a first-person shooter video game developed by Ubisoft Montreal and Ubisoft Toronto and published by Ubisoft for Microsoft Windows, PlayStation 4 and Xbox One. It is the standalone successor to the 2014 video game Far Cry 4, and the fifth main installment in the Far Cry series. The game was released on March 27, 2018. The game takes place in Hope County, a fictional region of Montana, United States. The main story revolves around the Project at Eden's Gate, a doomsday cult that has taken over the county at the command of its charismatic and powerful leader, Joseph Seed. Players control an unnamed junior deputy sheriff who becomes trapped in Hope County, and must work alongside factions of a resistance to liberate the county from the despotic rule of the Seeds and Eden's Gate. Gameplay focuses on combat and exploration; players battle enemy soldiers and dangerous wildlife using a wide array of weapons. The game features many elements found in role-playing games, such as a branching storyline and side quests. The game also features a map editor, a co-operative multiplayer mode, and a competitive multiplayer mode. Announced in early 2017, development on Far Cry 5 was extensive. The team explored several concepts before settling on an American location. The game was heavily inspired by several socio-political events in modern history, such as the Cold War and the September 11 attacks. The development team sought to capture the despondent social climate after the events and re-purpose it for the game. Developed and published solely by Ubisoft, its competitive multiplayer mode was also created in-house, with the company's worldwide studios gaining more creative input for Far Cry 5. Far Cry 5 was met with mostly positive critical reception upon release, although was the subject of controversy after being announced alongside a period of heightened political conflicts. Critics praised the open world design, visuals, and soundtrack, but directed criticisms towards its story and some of the characters. The game was a commercial success and became the fastest-selling title in the franchise, grossing over $310 million in its first week of sales. Several downloadable content packs have been released. A spin-off title and sequel to the narrative, Far Cry New Dawn, was released in February 15, 2019.</p>
-      </div>
+        <h3>Long Description</h3>
+        <p><?php echo $resultGameQuery['longDescription']; ?></p>
+      </div><!-- overview end -->
 
 
       <!-- Reviews section -->
       <div id="reviews" class="d-none">
-          <small style="color: #999;">Will Randomly select 6 Items</small>
+        <small style="color: #999;">Will Randomly select 6 Items</small>
+        <div class="row">
+        <div class="col-md-6">
+          <h3>Users</h3><!-- random order and set amount -->
+          <?php
+          foreach ($resultUserReview as $row){
+          ?>
           <div class="row">
-          <div class="col-md-6">
-            <h3>Users</h3><!-- random order and set amount -->
-            <div class="row">
-              <div class="col-md-3">
-                <img class="w-100 rounded" src="./img/profileIMG/11e79c5bd7cf103d.png" alt="Profile Image">
-              </div>
-              <div class="col-md-5">
-                <h5><span style="color: #999;">Name: </span><span>Ayden</span></h5>
-              </div>
-              <div class="col-md-4">
-                <h5><span style="color: #999;">Date: </span><span>09/08/2001</span></h5>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-3">
-                <h5>Comment: </h5>
-              </div>
-              <div class="col-md-9">
-                <p>great game</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <h3>Guests</h3><!-- random order and set amount -->
-            <div class="row">
-              <div class="col-md-3">
-                <img class="w-100 rounded" src="./img/500px%20icon.png" alt="Profile Image">
-              </div>
-              <div class="col-md-5">
-                <h5><span style="color: #999;">Name: </span><span>Ayden</span></h5>
-              </div>
-              <div class="col-md-4">
-                <h5><span style="color: #999;">Date: </span><span>09/08/2001</span></h5>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-3">
-                <h5>Comment: </h5>
-              </div>
-              <div class="col-md-9">
-                <p>great game</p>
-              </div>
-            </div>
-          </div>
-        </div>
+            <div class="col-md-3">
+              <img class="w-100 rounded" src="./img/profileIMG/<?php echo $row['UIMG']; ?>" alt="Profile Image">
+            </div><!-- col-md-3 end -->
+            <div class="col-md-5">
+              <h5><span style="color: #999;">Name: </span><span><?php echo $row['displayName']; ?></span></h5>
+            </div><!-- col-md-5 end -->
+            <div class="col-md-4">
+              <p><span style="color: #999;">Date: </span><span><?php echo $row['date']; ?></span></p>
+            </div><!-- col-md-4 end -->
+          </div><!--row end -->
+          <div class="row">
+            <div class="col-md-3">
+              <h5>Comment: </h5>
+            </div><!-- col-md-3 end -->
+            <div class="col-md-9">
+              <p><?php echo $row['comment']; ?></p>
+            </div><!-- col-md-9 end -->
+          </div><!-- row end -->
+          <?php } ?>
+        </div><!-- col-md-6 end -->
+        <div class="col-md-6">
+          <h3>Guests</h3><!-- random order and set amount -->
+          <div class="row">
+            <div class="col-md-3">
+              <img class="w-100 rounded" src="./img/500px%20icon.png" alt="Profile Image">
+            </div><!-- col-md-3 end -->
+            <div class="col-md-5">
+              <h5><span style="color: #999;">Name: </span><span>Ayden</span></h5>
+            </div><!-- col-md-5 end -->
+            <div class="col-md-4">
+              <p><span style="color: #999;">Date: </span><span>09/08/2001</span></p>
+            </div><!-- col-md-4 end -->
+          </div><!-- row end -->
+          <div class="row">
+            <div class="col-md-3">
+              <h5>Comment: </h5>
+            </div><!-- col-md-3 end -->
+            <div class="col-md-9">
+              <p>great game</p>
+            </div><!--col-md-9 end -->
+          </div><!-- row end -->
+        </div><!-- col-md-6 end -->
+        <hr>
         <?php
           if (!empty($_SESSION['lid'] && $_SESSION['cdid'] && $_SESSION['cpid'] && $_SESSION['start'] && $_SESSION['user'])) {
             ?>
-              <div id="userForm">
+              <div class="container" id="userForm">
+                <h4>Create a Review</h4>
+                <div class="row">
+                  <div class="col-2"></div><!-- empty div for centering -->
+                  <div class="col-8 form-group">
+                    <label for="userComment">Enter your comment</label>
+                    <textarea type="text" id="userComment" style="margin-bottom: 5px;" class="form-control" placeholder="Enter Comment"></textarea>
+                    <input id="formGIID" type="hidden" value="<?php echo $_GET['GIID']; ?>">
+                    <a class="btn btn-outline-primary" id="btnUserComment">Update<span style="height:15px; width:15px; margin-right: 10px;" id="spinner0"></span></a>
+                  </div><!-- col-8 end -->
+                  <div class="col-2"></div>
+                </div><!-- row end -->
               </div><!-- user Form end -->
             <?php
           } else {
             ?>
-              <div id="guestForm">
+              <div class="container" id="guestForm">
+                <h4>Create a Review</h4>
               </div><!-- guest Form end -->
             <?php
           }
         ?>
-      </div>
+      </div><!-- row end -->
+      </div><!-- reviews end -->
 
 
       <!-- Specification section -->
       <div id="specification" class="row d-none">
         <div class="col-md-6">
           <div class="row">
-            <div class="col-md-4"></div><!-- empty dive to force kinda center -->
-            <div class="col-md-4">
-              <p><span style="color: #999;">Platform: </span><span>XBOX</span></p>
-              <p><span style="color: #999;">Genre: </span><span>XBOX</span></p>
+            <div class="col-md-2"></div><!-- empty dive to force kinda center -->
+            <div class="col-md-6">
+              <p>
+                <span style="color: #999;">Platform: </span>
+                <span>
+                  <?php
+                    if ($resultGameQuery['platForm'] == 1){
+                      echo "Play Station";
+                    } elseif ($resultGameQuery['platForm'] == 2) {
+                      echo "XBOX";
+                    } elseif ($resultGameQuery['platForm'] == 3) {
+                      echo "Nintendo";
+                    } elseif ($resultGameQuery['platForm'] == 4) {
+                      echo "Personal Computer";
+                    } elseif ($resultGameQuery['platForm'] == 6) {
+                      echo "Works on all systems";
+                    } else {
+                      echo "Google it";
+                    }
+                  ?>
+                </span>
+              </p>
+              <p>
+                <span style="color: #999;">Genre: </span><span><?php echo $resultGameQuery['genre']; ?></span></p>
             </div><!-- col-md-4 end -->
-            <div class="col-md-4"></div><!-- empty dive to force kinda center -->
+            <div class="col-md-2"></div><!-- empty dive to force kinda center -->
           </div>
         </div>
         <div class="col-md-6">
           <div class="row">
-            <div class="col-md-4"></div><!-- empty dive to force kinda center -->
-            <div class="col-md-4">
-              <p><span style="color: #999;">Producer: </span><span>XBOX</span></p>
-              <p><span style="color: #999;">Requirements: </span><span>XBOX</span></p>
+            <div class="col-md-2"></div><!-- empty dive to force kinda center -->
+            <div class="col-md-6">
+              <p><span style="color: #999;">Producer: </span><span><?php echo $resultGameQuery['producer']; ?></span></p>
+              <p><span style="color: #999;">Requirements: </span><span><?php echo $resultGameQuery['Requirements']; ?></span></p>
             </div><!-- col-md-4 end -->
-            <div class="col-md-4"></div><!-- empty dive to force kinda center -->
+            <div class="col-md-2"></div><!-- empty dive to force kinda center -->
           </div>
         </div>
       </div>
